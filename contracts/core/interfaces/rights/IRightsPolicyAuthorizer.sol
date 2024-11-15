3ae069503b7ccf2c0c9fc449108699cfeec41c6f
Geolffrey Mena <gmjun2000@gmail.com>
2024-11-15 11:39:20 -0600

    chore: remove old addresses

---
+++ contracts/core/interfaces/rights/IRightsPolicyAuthorizer.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+/// @title IRightsPolicyAuthorizer
+/// @notice Interface for authorizing and managing policies related to content rights.
+/// @dev This interface allows content holders to authorize and revoke policies
+/// that manage their content-related rights.
+interface IRightsPolicyAuthorizer {
+    /// @notice Retrieves all policies authorized by a specific content holder.
+    /// @dev This function returns an array of policy addresses that have been granted rights by the holder.
+    /// @param holder The address of the asset rights holder whose authorized policies are being queried.
+    function getAuthorizedPolicies(address holder) external view returns (address[] memory);
+
+    /// @notice Checks if a specific policy contract has been authorized by a content holder.
+    /// @dev Verifies if the specified policy has permission to manage rights for the given holder’s content.
+    /// @param policy The address of the policy contract to check.
+    /// @param holder The address of the asset rights holder to verify authorization.
+    function isPolicyAuthorized(address policy, address holder) external view returns (bool);
+
+    /// @notice Authorizes a policy contract, granting it rights to manage the asset associated with the holder.
+    /// @dev This function allows a content holder to authorize a policy to manage its content rights.
+    /// @param policy The address of the policy contract to authorize.
+    /// @param data The data to initialize policy.
+    function authorizePolicy(address policy, bytes calldata data) external;
+
+    /// @notice Revokes the authorization of a policy contract.
+    /// @dev This function removes the rights of a policy, preventing it from managing the asset holder’s assets.
+    /// @param policy The address of the policy contract to revoke authorization.
+    function revokePolicy(address policy) external;
+}
---
+++ contracts/core/interfaces/rights/IRightsPolicyManager.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+/// @title IRightsPolicyManager
+/// @notice Interface for managing content rights policies.
+/// @dev This interface handles retrieving/managing/registering policies.
+interface IRightsPolicyManager {
+    /// @notice Retrieves the list of policies associated with a specific account and content ID.
+    /// @param account The address of the account for which policies are being retrieved.
+    function getPolicies(address account) external view returns (address[] memory);
+
+    /// @notice Retrieves the address of the Rights Policies Authorizer contract.
+    /// @return The address of the contract responsible for authorizing rights policies.
+    function getPolicyAuthorizer() external view returns (address);
+
+    /// @notice Retrieves the first active policy for a specific account in LIFO order.
+    /// @param account The address of the account to evaluate.
+    /// @param contentId The identifier of the content to validate the policy status.
+    function getActivePolicy(address account, uint256 contentId) external view returns (bool, address);
+
+    /// @notice Finalizes the agreement by registering the agreed-upon policy, effectively closing the agreement.
+    /// @param proof The unique identifier of the agreement to be enforced.
+    /// @param holder The rights holder whose authorization is required for accessing the asset.
+    /// @param policyAddress The address of the policy contract managing the agreement.
+    function registerPolicy(uint256 proof, address holder, address policyAddress) external returns (uint256);
+
+    /// @notice Verifies if a specific policy is active for the provided account and content.
+    /// @param account The address of the user whose compliance is being evaluated.
+    /// @param contentId The identifier of the content to validate the policy status.
+    /// @param policyAddress The address of the policy contract to check compliance against.
+    function isActivePolicy(address account, uint256 contentId, address policyAddress) external view returns (bool);
+}
