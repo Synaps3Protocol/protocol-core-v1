24160b1ee837a8c9e51a2323507d1b8d80807b56
Geolffrey Mena <gmjun2000@gmail.com>
2024-11-20 15:07:43 -0600

    refactor: rename final ops

--- packages/protocol/contracts/core/interfaces/policies/IPolicyAuditor.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
-
-import { IPolicyAuditorRegistrable } from "@synaps3/core/interfaces/policies/IPolicyAuditorRegistrable.sol";
-import { IPolicyAuditorVerifiable } from "@synaps3/core/interfaces/policies/IPolicyAuditorVerifiable.sol";
-
-/// @title IPolicyAuditor
-/// @notice Interface for managing the registration and verification of policies auditors within the system.
-interface IPolicyAuditor is IPolicyAuditorRegistrable, IPolicyAuditorVerifiable {}
