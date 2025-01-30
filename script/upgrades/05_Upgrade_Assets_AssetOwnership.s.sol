5463796ea812b45f1d28fabe09a841bdf52b438e
Geolffrey Mena <gmjun2000@gmail.com>
2025-01-29 18:55:41 -0600

    docs: economic model paper

---
+++ script/upgrades/05_Upgrade_Assets_AssetOwnership.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { UpgradeBase } from "script/upgrades/00_Upgrade_Base.s.sol";
+import { AssetOwnership } from "contracts/assets/AssetOwnership.sol";
+import { C } from "contracts/core/primitives/Constants.sol";
+
+contract UpgradeAssetOwnership is UpgradeBase {
+    function run() external returns (address) {
+        vm.startBroadcast(getAdminPK());
+        address assetReferendum = vm.envAddress("ASSET_REFERENDUM");
+        address impl = address(new AssetOwnership(assetReferendum));
+        address assetOwnershipProxy = vm.envAddress("ASSET_OWNERSHIP");
+        // address accessManager = vm.envAddress("ACCESS_MANAGER");
+        //!IMPORTANT: This is not a safe upgrade, take any caution or 2-check needed before run this method
+        // bytes memory init = abi.encodeCall(LedgerVaultV2.initializeV2, (accessManager));
+        address assetOwnership = upgradeAndCallUUPS(assetOwnershipProxy, impl, ""); // no initialization
+        vm.stopBroadcast();
+        return assetOwnership;
+    }
+}
---
+++ script/upgrades/06_Upgrade_Rights_RightsPolicyAuthorizer.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { UpgradeBase } from "script/upgrades/00_Upgrade_Base.s.sol";
+import { RightsPolicyAuthorizer } from "contracts/rights/RightsPolicyAuthorizer.sol";
+import { C } from "contracts/core/primitives/Constants.sol";
+
+contract UpgradeRightsPolicyAuthorizer is UpgradeBase {
+    function run() external returns (address) {
+        vm.startBroadcast(getAdminPK());
+        address policyAuditor = vm.envAddress("POLICY_AUDIT");
+        address impl = address(new RightsPolicyAuthorizer(policyAuditor));
+        address policyAuthorizerProxy = vm.envAddress("RIGHT_POLICY_AUTHORIZER");
+        // address accessManager = vm.envAddress("ACCESS_MANAGER");
+        //!IMPORTANT: This is not a safe upgrade, take any caution or 2-check needed before run this method
+        // bytes memory init = abi.encodeCall(LedgerVaultV2.initializeV2, (accessManager));
+        address rightPolicyAuthorizer = upgradeAndCallUUPS(policyAuthorizerProxy, impl, ""); // no initialization
+        vm.stopBroadcast();
+        return rightPolicyAuthorizer;
+    }
+}
