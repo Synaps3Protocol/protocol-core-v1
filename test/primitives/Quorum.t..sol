b98ab1dd694f37d8cd8d19fb03eac0242e4a18a9
Geolffrey Mena <gmjun2000@gmail.com>
2025-04-07 11:46:42 -0600

    chore: temp disable sectest

--- test/primitives/Quorum.t..sol
+++ test/primitives/Quorum.t..sol
@@ -4,76 +4,102 @@ pragma solidity 0.8.26;
 import "forge-std/Test.sol";
 import { QuorumUpgradeable } from "contracts/core/primitives/upgradeable/QuorumUpgradeable.sol";
 
-contract QuorumTest is Test, QuorumUpgradeable {
+contract QuorumWrapper is QuorumUpgradeable {
+    function status(uint256 entry) external view returns (Status) {
+        return _status(entry);
+    }
+
+    function revoke(uint256 entry) external {
+        _revoke(entry);
+    }
+
+    function blocked(uint256 entry) external {
+        _block(entry);
+    }
+
+    function approve(uint256 entry) external {
+        _approve(entry);
+    }
+
+    function quit(uint256 entry) external {
+        _quit(entry);
+    }
+
+    function register(uint256 entry) external {
+        _register(entry);
+    }
+}
+
+contract QuorumTest is Test {
+    QuorumWrapper quorum;
+
+    function setUp() public {
+        quorum = new QuorumWrapper();
+    }
+
     function test_DefaultStatus() public view {
-        Status status = _status(1234536789);
-        assertEq(uint(status), 0);
+        assertEq(uint(quorum.status(1234536789)), 0);
     }
 
     function test_RegisterStatusFlow() public {
         uint256 entry = 1234567189;
         // initial pending status
-        Status status = _status(entry);
-        assertEq(uint(status), 0);
+        assertEq(uint(quorum.status(entry)), 0);
 
         // register status
-        _register(entry);
-        Status waitingStatus = _status(entry);
-        assertEq(uint(waitingStatus), 1);
+        quorum.register(entry);
+        assertEq(uint(quorum.status(entry)), 1);
     }
 
     function test_ActiveStatusFlow() public {
         uint256 entry = 1234526789;
         // waiting status
-        _register(entry);
+        quorum.register(entry);
         // active status
-        _approve(entry);
-        Status activeStatus = _status(entry);
-        assertEq(uint(activeStatus), 2);
+        quorum.approve(entry);
+        assertEq(uint(quorum.status(entry)), 2);
     }
 
     function test_QuitStatusFlow() public {
         uint256 entry = 1234256789;
         // waiting status
-        _register(entry);
+        quorum.register(entry);
         // pending status
-        _quit(entry);
-        Status activeStatus = _status(entry);
-        assertEq(uint(activeStatus), 0);
+        quorum.quit(entry);
+        assertEq(uint(quorum.status(entry)), 0);
     }
 
     function test_BlockedStatusFlow() public {
         uint256 entry = 123455589;
         // waiting status
-        _register(entry);
+        quorum.register(entry);
         // active status
-        _approve(entry);
+        quorum.approve(entry);
         // blocked status
-        _revoke(entry);
-        Status blockedStatus = _status(entry);
-        assertEq(uint(blockedStatus), 3);
+        quorum.revoke(entry);
+        assertEq(uint(quorum.status(entry)), 3);
     }
 
     function test_Approve_RevertWhen_ApproveNotRegistered() public {
         uint256 entry = 123456789;
         // active status
-        vm.expectRevert(NotWaitingApproval.selector);
-        _approve(entry);
+        vm.expectRevert(QuorumUpgradeable.NotWaitingApproval.selector);
+        quorum.approve(entry);
     }
 
     function test_Revoke_RevertWhen_BlockedNotActive() public {
         uint256 entry = 12345677;
         // waiting status
-        _register(entry);
+        quorum.register(entry);
         // blocked status
-        vm.expectRevert(InvalidInactiveState.selector);
-        _revoke(entry);
+        vm.expectRevert(QuorumUpgradeable.InvalidInactiveState.selector);
+        quorum.revoke(entry);
     }
 
     function test_Quit_RevertWhen_QuitNotWaiting() public {
         uint256 entry = 123456459;
         // blocked status
-        vm.expectRevert(NotWaitingApproval.selector);
-        _quit(entry);
+        vm.expectRevert(QuorumUpgradeable.NotWaitingApproval.selector);
+        quorum.quit(entry);
     }
 }
