bb0d6695b3b01fa8e5d0f088cc27c33335e3e1c8
Geolffrey Mena <gmjun2000@gmail.com>
2025-05-12 15:03:56 -0600

    refactor(custodian): removed erc165 checker

--- contracts/custody/CustodianReferendum.sol
+++ contracts/custody/CustodianReferendum.sol
@@ -2,7 +2,6 @@
 // NatSpec format convention - https://docs.soliditylang.org/en/v0.5.10/natspec-format.html
 pragma solidity 0.8.26;
 
-import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 // solhint-disable-next-line max-line-length
@@ -30,7 +29,6 @@ contract CustodianReferendum is
     IFeeSchemeValidator
 {
     using FinancialOps for address;
-    using ERC165Checker for address;
 
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     IAgreementSettler public immutable AGREEMENT_SETTLER;
