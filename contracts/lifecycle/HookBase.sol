b841b9c2a70050d04cc2acb7540324992695e07f
Geolffrey Mena <gmjun2000@gmail.com>
2025-05-13 14:21:42 -0600

    refactor(custodian): removed erc165 checker

--- contracts/lifecycle/HookBase.sol
+++ contracts/lifecycle/HookBase.sol
@@ -2,9 +2,23 @@
 // NatSpec format convention - https://docs.soliditylang.org/en/v0.5.10/natspec-format.html
 pragma solidity 0.8.26;
 
-// se podria aprovechar la logica de los modifier para establecer control de ejecucion antes y despues
-// crear un onCall modifier para agregar a los metodo y registrar en base a cada hook acciones especificas en el llamado
+import { IHook } from "contracts/core/interfaces/lifecycle/IHook.sol";
 
 // pre - validate
 // during - execute
 // pos - verify
+
+abstract contract HookBase is IHook {
+    // pre call to check if the hook its associated with the target
+    // eg. SubscriptionPolicy its allowed to process this hook
+    function validate(address caller, address target) external returns (bool) {}
+
+    /// @notice Called by the protocol to signal the use of the hook
+    /// @param caller The address invoking the hook (e.g., the user or distributor)
+    /// @param context Optional: bytes data for hook-specific usage
+    function execute(address caller, bytes calldata context) external returns (bytes memory) {}
+
+    // post call to verify the execution, run logs, etc
+    // eg. SubscriptionPolicy execution complain well?
+    function verify(address caller, bytes calldata results) external returns (bool) {}
+}
