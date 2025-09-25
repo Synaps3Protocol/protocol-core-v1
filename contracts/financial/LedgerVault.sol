846a6e2fe2e98e1bec1f903b50c26ed6223bfcc7
Geolffrey Mena <gmjun2000@gmail.com>
2025-09-25 09:29:06 -0600

    removed the uneeded exceed

--- contracts/financial/LedgerVault.sol
+++ contracts/financial/LedgerVault.sol
@@ -113,7 +113,7 @@ contract LedgerVault is
     /// @param recipient The address of the account to credit with the deposit.
     /// @param amount The amount of currency to deposit.
     /// @param currency The address of the ERC20 token to deposit.
-    function deposit(address recipient, uint256 amount, address currency) external whenNotPaused returns (uint256) {
+    function deposit(address recipient, uint256 amount, address currency) external payable whenNotPaused returns (uint256) {
         return _deposit(recipient, amount, currency);
     }
 
